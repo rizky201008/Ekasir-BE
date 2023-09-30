4073f596bbb244647c0d265423f2aff3702f976b
Rizky Agung Prayogi <rizkyden06@gmail.com>
2023-09-30 13:23:50 +0700

    create new route

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\TransactionController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +19,13 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::group(['prefix' => '/admin'], function () {
+    Route::get('/products', [ProductController::class, 'getProducts']);
+    Route::post('/products', [ProductController::class, 'insertProduct']);
+    Route::get('/categories', [CategoryController::class, 'getCategories']);
+    Route::post('/categories', [CategoryController::class, 'insertCategory']);
+    Route::get('/transactions', [TransactionController::class, 'getTransactions']);
+    Route::post('/transactions', [TransactionController::class, 'insertTransaction']);
 });

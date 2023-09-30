83acd7de79cecaf80c1f0ce1ed984fa64d101558
Rizky Agung Prayogi <rizkyden06@gmail.com>
2023-09-30 13:21:16 +0700

    create and modify Factory

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Product>
+ */
+class ProductFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => fake()->name(),
+            'price_1' => rand(100000,500000),
+            'category_id' => rand(1,10),
+            'stock' => rand(1,1000)
+        ];
+    }
+}
---
+++ database/factories/TransactionFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Support\Str;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Transaction>
+ */
+class TransactionFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'price' => rand(1000000, 5000000),
+            'product' => fake()->name(),
+            'amount' => rand(1,5),
+            'trxid' => Str::random(10),
+        ];
+    }
+}

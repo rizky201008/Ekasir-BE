fc542ef705fe52b77e07d057df8c2a56101faa33
Rizky Agung Prayogi <rizkyden06@gmail.com>
2023-10-05 11:34:22 +0700

    create new controller

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    function login(Request $req)
+    {
+        $req->validate([
+            'email' => 'required|email',
+            'password' => 'required'
+        ]);
+
+        if (Auth::attempt([
+            'email' => $req->email,
+            'password' => $req->password
+        ])) {
+            $user = User::where('email', $req->email)->first();
+            $token = $user->createToken();
+
+            return response()->json([
+                'token' => $token->plainTextToken
+            ], 201);
+        }
+    }
+
+    function register(Request $req)
+    {
+        $req->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+            'name' => 'required'
+        ]);
+
+        User::create([
+            'email' => $req->email,
+            'password' => $req->password,
+            'name' => $req->name
+        ]);
+
+        return response()->json([
+            'message' => 'Successfuly register!'
+        ], 201);
+    }
+
+    function logout(Request $req)
+    {
+        // Revoke the token that was used to authenticate the current request...
+        $req->user()->currentAccessToken()->delete();
+
+        return response()->json([
+            'message' => 'Successfully logged out!'
+        ], 200);
+    }
+
+    function forgotPassword()
+    {
+        
+    }
+}

13e0eb745861d1b302b34977b342619ac06c4a04
Rizky Agung Prayogi <rizkyden06@gmail.com>
2023-10-06 12:30:13 +0700

    modify migrations

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -26,7 +26,8 @@ return new class extends Migration
     /**
      * Reverse the migrations.
      */
-    public function down(): void
+    
+     public function down(): void
     {
         Schema::dropIfExists('users');
     }

30018e2ad571d0121032229617bef8bb6fd84906
Rizky Agung Prayogi <rizkyden06@gmail.com>
2023-10-23 10:13:39 +0700

    add new route

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Livewire\Home;
+use App\Livewire\Registration;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,3 +16,4 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', Home::class);
+Route::get('/register', Registration::class);
